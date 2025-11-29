ed6880b1d5a37429e5492a2a38f8b412a90d9882
IvanR96 <irios96@hotmail.com>
2025-11-28 23:24:05 -0800

    main application creation

---
+++ keeper/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>React App</h1>
+  </div>
+)
