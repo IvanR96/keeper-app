05d6f207646487419469085073f16f630f558dc2
IvanR96 <irios96@hotmail.com>
2025-11-29 00:03:56 -0800

    app imported to main

--- keeper/src/main.jsx
+++ keeper/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
-  <div>
-    <h1>React App</h1>
-  </div>
+  <App />
 )
