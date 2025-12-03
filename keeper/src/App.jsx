f630ba55e6e2e2c09719043a25e2708ad072c381
IvanR96 <irios96@hotmail.com>
2025-12-02 23:33:15 -0800

    card component added to app jsx

--- keeper/src/App.jsx
+++ keeper/src/App.jsx
@@ -3,12 +3,17 @@ import './index.css'
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import Card from './Card';
 
 function App(){
   return <div>
     <Header />
     <Footer />
     <Note />
+    <Card 
+    name="Joe Smith"
+    img=""
+    />
   </div>
 };
 
