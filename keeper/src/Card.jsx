2a55d54e079554b67e56fb7b50be7de12f12c01c
IvanR96 <irios96@hotmail.com>
2025-12-03 21:46:32 -0800

    card component changed

--- keeper/src/Card.jsx
+++ keeper/src/Card.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Card(props){
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <img src="{props.img}" alt="avatar_img"/>
-            <p>{props.tel}</p>
-            <p>{props.email}</p>
+        <div className="card">
+            <h2 className="name">{props.name}</h2>
+            <img className="circle-img" src={props.img} alt="avatar_img"/>
+            <p className="info">{props.tel}</p>
+            <p className="info">{props.email}</p>
         </div>
     )
 };
