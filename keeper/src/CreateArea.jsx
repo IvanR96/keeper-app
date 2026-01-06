ceff37d7547da2f6f1e4ae471bf047e162a1292e
IvanR96 <irios96@hotmail.com>
2026-01-05 20:33:38 -0800

    create area done

--- keeper/src/CreateArea.jsx
+++ keeper/src/CreateArea.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react"
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
 
 function CreateArea(props) {
+    const [isExpanded, setExpanded] = useState(false)
+
      const [note, setNote] = useState({
         title: "",
         content: ""
@@ -16,6 +21,10 @@ function CreateArea(props) {
       };
     });
   }
+
+    const expand = () =>{
+      setExpanded(true)
+    }
 
     
 
@@ -35,10 +44,13 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form>
-        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
-        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote}>Add</button>
+      <form className="create-note">
+        {isExpanded ? 
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" /> : null}
+        <textarea name="content" onChange={handleChange} onClick={expand} value={note.content} placeholder="Take a note..." rows={isExpanded ? 3 : 1} />
+        <Zoom in={isExpanded}>
+        <Fab onClick={submitNote}> <AddIcon/></Fab>
+        </Zoom>
       </form>
     </div>
   );
