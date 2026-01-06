15f7024de6fba00d9a9e7b2d72834c3db1e121d7
IvanR96 <irios96@hotmail.com>
2026-01-05 20:34:35 -0800

    note component

--- keeper/src/Note.jsx
+++ keeper/src/Note.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 
 
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
 
-function Note(props){
-
-    return <div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button className="delete" onClick={handleClick}><DeleteIcon/></button>
     </div>
-};
+  );
+}
 
 export default Note;
